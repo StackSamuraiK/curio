2b2f7e67a4e97e49bbaa05179760b235b1e53c83
StackSamuraiK <kshitizsrivastav3344@gmail.com>
2025-05-13 20:46:11 +0530

    added_agent_route

--- src/student/index.ts
+++ src/student/index.ts
@@ -1,6 +1,11 @@
 import express from "express"
 import { Request , Response } from "express"
+import authRouter from "./routes/auth"
+import agentRouter from "./routes/agent"
 
 const studentRouter = express.Router()
+
+studentRouter.use('/auth' , authRouter)
+studentRouter.use('/agent' , agentRouter)
 
 export default studentRouter
---
+++ src/student/routes/agent.ts
@@ -0,0 +1,31 @@
+import { GenerateContentResponse, GoogleGenAI } from "@google/genai";
+import expresd from "express"
+import { Request, Response } from "express";
+
+const agentRouter = expresd.Router()
+
+agentRouter.get('/test', async (req: Request, res: Response) => {
+    const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API });
+
+    async function main() {
+        const response = await ai.models.generateContentStream({
+            model: "gemini-2.0-flash",
+            contents: "Explain how AI works in a 100 words in hinglish",
+            config: {
+                systemInstruction: "You are an AI expert and you are giving a lecture in one of the most prestigious college of the Universe IIT Bombaby.",
+                maxOutputTokens: 1000,
+                temperature: 0.8
+            },
+
+        });
+
+        for await (const chunk of response) {
+            console.log(chunk.text)
+            res.write(chunk.text)
+        }
+    }
+    await main();
+})
+
+
+export default agentRouter
